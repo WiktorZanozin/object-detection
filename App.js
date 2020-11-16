40ea074c1d7bf15a3d200a557b34972ab9fa5a7f
Viktor Zanozin <wiktor.zanozin@gmail.com>
2020-11-16 19:27:23 +0100

    Added screen architecture

--- App.js
+++ App.js
@@ -1,266 +1,69 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, StatusBar, ActivityIndicator, TouchableOpacity, Image } from 'react-native'
-import { ScrollView } from 'react-native-gesture-handler';
+import * as React from 'react';
+import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import { SplashScreen } from 'expo';
+import * as Font from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-import Constants from 'expo-constants'
-import * as Permissions from 'expo-permissions'
-import * as ImagePicker from 'expo-image-picker'
-import * as FileSystem from 'expo-file-system'
+import BottomTabNavigator from './navigation/BottomTabNavigation';
+import useLinking from './navigation/useLinking';
 
-import * as jpeg from 'jpeg-js'
+const Stack = createStackNavigator();
 
-import * as tf from '@tensorflow/tfjs';
-import '@tensorflow/tfjs-react-native';
+console.disableYellowBox = true;
 
-// import * as mobilenet from '@tensorflow-models/mobilenet'
-// You can try with other models, see https://github.com/tensorflow/tfjs-models
-import * as cocossd from '@tensorflow-models/coco-ssd'
+export default function App(props) {
+  const [isLoadingComplete, setLoadingComplete] = React.useState(false);
+  const [initialNavigationState, setInitialNavigationState] = React.useState();
+  const containerRef = React.useRef();
+  const { getInitialState } = useLinking(containerRef);
 
-export default class CocoSsdScreen extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isTfReady: false,
-      isModelReady: false,
-      predictions: null,
-      image: null
-    };
-  }
+  // Load any resources or data that we need prior to rendering the app
+  React.useEffect(() => {
+    async function loadResourcesAndDataAsync() {
+      try {
+        SplashScreen.preventAutoHide();
 
-  async componentDidMount() {
-    await tf.ready(); // preparing TensorFlow
-    this.setState({ isTfReady: true,});
+        // Load our initial navigation state
+        setInitialNavigationState(await getInitialState());
 
-    // this.model = await mobilenet.load(); // preparing MobileNet model
-    this.model = await cocossd.load(); // preparing COCO-SSD model
-    this.setState({ isModelReady: true });
-
-    this.getPermissionAsync(); // get permission for accessing camera on mobile device
-  }
-
-  getPermissionAsync = async () => {
-    if (Constants.platform.ios) {
-        const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL)
-        if (status !== 'granted') {
-            alert('Please grant camera roll permission for this project!')
-        }
-    }
-  }
-
-  imageToTensor(rawImageData) {
-    const TO_UINT8ARRAY = true
-    const { width, height, data } = jpeg.decode(rawImageData, TO_UINT8ARRAY)
-    // Drop the alpha channel info for mobilenet
-    const buffer = new Uint8Array(width * height * 3)
-    let offset = 0 // offset into original data
-    for (let i = 0; i < buffer.length; i += 3) {
-      buffer[i] = data[offset]
-      buffer[i + 1] = data[offset + 1]
-      buffer[i + 2] = data[offset + 2]
-
-      offset += 4
-    }
-
-    return tf.tensor3d(buffer, [height, width, 3])
-  }
-
-  detectObjects = async () => {
-    try {
-      const imageAssetPath = Image.resolveAssetSource(this.state.image)
-
-      console.log(imageAssetPath.uri);
-      const imgB64 = await FileSystem.readAsStringAsync(imageAssetPath.uri, {
-      	encoding: FileSystem.EncodingType.Base64,
-      });
-      const imgBuffer = tf.util.encodeString(imgB64, 'base64').buffer;
-      const raw = new Uint8Array(imgBuffer)
-      const imageTensor = this.imageToTensor(raw);
-      console.log('imageTensor: ', imageTensor);
-      const predictions = await this.model.detect(imageTensor)
-
-      this.setState({ predictions: predictions })
-
-
-      console.log('----------- predictions: ', predictions);
-
-    } catch (error) {
-      console.log('Exception Error: ', error)
-    }
-  }
-
-  selectImage = async () => {
-    try {
-      let response = await ImagePicker.launchImageLibraryAsync({
-        mediaTypes: ImagePicker.MediaTypeOptions.All,
-        allowsEditing: true,
-        aspect: [4, 3]
-      })
-
-      if (!response.cancelled) {
-        const source = { uri: response.uri }
-        this.setState({ image: source })
-        this.detectObjects()
+        // Load fonts
+        await Font.loadAsync({
+          ...Ionicons.font,
+          'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
+        });
+      } catch (e) {
+        // We might want to provide this error information to an error reporting service
+        console.warn(e);
+      } finally {
+        setLoadingComplete(true);
+        SplashScreen.hide();
       }
-    } catch (error) {
-      console.log(error)
     }
-  }
 
-  /*
-  [{
-  bbox: [x, y, width, height],
-  class: "person",
-  score: 0.8380282521247864
-  }, {
-  bbox: [x, y, width, height],
-  class: "kite",
-  score: 0.74644153267145157
-  }]
-  */
-  renderPrediction = (prediction, index) => {
-    const pclass = prediction.class;
-    const score  = prediction.score;
-    const x = prediction.bbox[0];
-    const y = prediction.bbox[1];
-    const w = prediction.bbox[2];
-    const h = prediction.bbox[3];
+    loadResourcesAndDataAsync();
+  }, []);
 
-    return (
-        <Text  key={index} style={styles.text}>
-          Prediction: {pclass} {', '} Probability: {score} {', '} Bbox: {x} {', '} {y} {', '} {w} {', '} {h}
-        </Text>
-    )
-  }
-
-  render() {
-    const { isTfReady, isModelReady, predictions, image } = this.state
-
+  if (!isLoadingComplete && !props.skipLoadingScreen) {
+    return null;
+  } else {
     return (
       <View style={styles.container}>
-      <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-
-        <View style={styles.welcomeContainer}>
-         
-        <StatusBar barStyle='light-content' />
-        <View style={styles.loadingContainer}>
-          <Text style={styles.text}>
-            TensorFlow.js ready? {isTfReady ? <Text>✌</Text> : ''}
-          </Text>
-
-          <View style={styles.loadingModelContainer}>
-            <Text style={styles.text}>COCO-SSD model ready? </Text>
-            {isModelReady ? (
-              <Text style={styles.text}>✌</Text>
-            ) : (
-              <ActivityIndicator size='small' />
-            )}
-          </View>
-        </View>
-        <TouchableOpacity
-          style={styles.imageWrapper}
-          onPress={isModelReady ? this.selectImage : undefined}>
-          {image && <Image source={image} style={styles.imageContainer} />}
-
-          {isModelReady && !image && (
-            <Text style={styles.transparentText}>Tap to choose image</Text>
-          )}
-        </TouchableOpacity>
-        <View style={styles.predictionWrapper}>
-          {isModelReady && image && (
-            <Text style={styles.text}>
-              Predictions: {predictions ? '' : 'Detecting...'}
-            </Text>
-          )}
-
-          {isModelReady &&
-            predictions &&
-            predictions.map((p, index) => this.renderPrediction(p, index))}
-        </View>
-        </View>
-        </ScrollView>
+        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+        <NavigationContainer ref={containerRef} initialState={initialNavigationState}>
+          <Stack.Navigator>
+            <Stack.Screen name="Root" component={BottomTabNavigator} />
+          </Stack.Navigator>
+        </NavigationContainer>
       </View>
-    )
+    );
   }
 }
-
-CocoSsdScreen.navigationOptions = {
-  header: null,
-};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#171f24'
+    backgroundColor: '#fff',
   },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  loadingContainer: {
-    marginTop: 20,
-    justifyContent: 'center'
-  },
-  text: {
-    color: '#ffffff',
-    fontSize: 16
-  },
-  loadingModelContainer: {
-    flexDirection: 'row',
-    marginTop: 10
-  },
-  imageWrapper: {
-    width: 280,
-    height: 280,
-    padding: 10,
-    borderColor: 'blue',
-    borderWidth: 5,
-    borderStyle: 'dashed',
-    marginTop: 40,
-    marginBottom: 10,
-    position: 'relative',
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  imageContainer: {
-    width: 250,
-    height: 250,
-    position: 'absolute',
-    top: 10,
-    left: 10,
-    bottom: 10,
-    right: 10
-  },
-  predictionWrapper: {
-    height: 100,
-    width: '100%',
-    flexDirection: 'column',
-    alignItems: 'center',
-    marginLeft : 20
-  },
-  transparentText: {
-    color: '#ffffff',
-    opacity: 0.7
-  },
-  footer: {
-    marginTop: 40
-  },
-  poweredBy: {
-    fontSize: 20,
-    color: '#e69e34',
-    marginBottom: 6
-  },
-  tfLogo: {
-    width: 125,
-    height: 70
-  }
-})
+});
---
+++ navigation/BottomTabNavigation.js
@@ -0,0 +1,60 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import * as React from 'react';
+
+import TabBarIcon from '../components/TabBarIcon';
+import HomeScreen from '../screens/HomeScreen';
+import ImageScreen from '../screens/ImageScreen';
+import CameraScreen from './../screens/CameraScreen';
+
+
+const BottomTab = createBottomTabNavigator();
+const INITIAL_ROUTE_NAME = 'Home';
+
+export default function BottomTabNavigator({ navigation, route }) {
+  // Set the header title on the parent stack navigator depending on the
+  // currently active tab. Learn more in the documentation:
+  // https://reactnavigation.org/docs/en/screen-options-resolution.html
+  navigation.setOptions({ headerTitle: getHeaderTitle(route) });
+
+  return (
+    <BottomTab.Navigator initialRouteName={INITIAL_ROUTE_NAME}>
+      <BottomTab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          title: 'Introduction',
+          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="ios-home" />,
+        }}
+      />
+      <BottomTab.Screen
+        name="ImageScreen"
+        component={ImageScreen}
+        options={{
+          title: 'Image Screen',
+          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="ios-image" />,
+        }}
+      />
+      <BottomTab.Screen
+        name="Links"
+        component={CameraScreen}
+        options={{
+          title: 'References',
+          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="ios-book" />,
+        }}
+      />
+    </BottomTab.Navigator>
+  );
+}
+
+function getHeaderTitle(route) {
+  const routeName = route.state?.routes[route.state.index]?.name ?? INITIAL_ROUTE_NAME;
+
+  switch (routeName) {
+    case 'Home':
+      return 'Introduction';
+    case 'ImageScreen':
+      return 'Image Screen';
+    case 'CameraScreen':
+      return 'Camera Screen';
+  }
+}
